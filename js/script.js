f41b30b6a9953a7b09e2d3be033faaca3e023bcb
Artem <trootootoo14@gmail.com>
2021-02-03 11:18:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+"use strict";
+// let a = 15;
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// {
+//    var result = 50;
+// }
+
+// console.log(result);
+
+// alert(`Hello`);
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18 years old or older?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your second name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Artem';
+
+// alert(`Hello, ${user}`);
