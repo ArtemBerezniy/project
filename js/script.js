19f760034ec2549a36001b6dab73556f7224d76a
Artem <trootootoo14@gmail.com>
2021-02-16 19:41:04 +0200

    Циклы

--- js/script.js
+++ js/script.js
@@ -42,7 +42,93 @@
 // alert(`Hello, ${user}`);
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Wrong');
+// }
+
+// const num = 48;
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// }else {
+//     console.log('ok');
+// }
+
+
+
+// const nums = 50;
+// (nums === 50) ? console.log('ok') : console.log('error');
+
+
+// const num = 50;
+
+
+// switch (num) {
+//     case 49:
+//         console.log('wrong');
+//         break;
+//     case 100:
+//         console.log('wrong');
+//         break;
+//     case 50:
+//         console.log('Super');
+//         break;
+//     default:
+//         console.log('Not this time');
+//         break;
+
+// }
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+//  for (let i = 1; i < 10; i++) {
+//      if (i === 6) {
+//         //  break;
+//         continue;
+//      }
+
+//     console.log(i);
+//  }
+
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -52,12 +138,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+   if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB[a] = b;
+      console.log('Super');
+   } else {
+      console.log('Wrong');
+      i--;
+   }
+}
+
+if (personalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log('ERRRROOOOORRRRR');
+}
 console.log(personalMovieDB);
